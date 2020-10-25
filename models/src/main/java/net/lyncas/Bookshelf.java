e37e7031d2c11f5116733d17542d18fe53bb34f6
cristopher <cristopher.ecruz@gmail.com>
2020-10-24 21:36:55 -0300

    initial commit

---
+++ models/src/main/java/net/lyncas/Bookshelf.java
@@ -0,0 +1,9 @@
+package net.lyncas;
+
+import java.util.List;
+
+public class Bookshelf {
+
+	private List<Volume> volumes;
+	
+}
---
+++ models/src/main/java/net/lyncas/Volume.java
@@ -0,0 +1,10 @@
+package net.lyncas;
+
+import java.util.UUID;
+
+public class Volume {
+
+	private UUID id;
+	private String volumeId;
+	
+}
